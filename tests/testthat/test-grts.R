d0a944c611c6bfc6c91bb394584746c65122b53a
Marc Weber <mweber36@gmail.com>
2019-07-15 11:45:27 -0700

    Update testing with Tom's files in extdata

--- tests/testthat/test-grts.R
+++ tests/testthat/test-grts.R
@@ -1,15 +1,29 @@
 context("grts")
 
-data("NE_lakes","Luck_Ash_streams", "UT_ecoregions")
-
 set.seed(52468110)
+# Create sp objects
+sp.finite <- read.shape(system.file("extdata", "reg1_lakes.shp", package="spsurvey"))
+sp.finite@data$mdcaty <- runif(nrow(sp.finite))
 
 testsample <- grts(design=list(None=list(panel=c(PanelOne=10), 
                                          over=0,seltype="Equal")),
-                   src.frame="sf.object", type.frame="finite", sf.object=NE_lakes,shapefile=TRUE)
+                   type.frame="finite", in.shape=system.file("extdata", "reg1_lakes.shp", package="spsurvey"),
+                   shapefile=TRUE)
 
-test_that("test finite grts design with output shapefile",{
+test_that("test finite design equal random selection using shapefile and output shapefile",{
   expect_true(file.exists('sample.shp'))
   expect_true(exists("testsample"))
   expect_equal(attributes(testsample)$class[1],"SpatialDesign")
+  expect_equal(nrow(testsample@data),10)
+})
+
+testsample <- grts(design=list(None=list(panel=c(PanelOne=10), over=0,
+                                         seltype="Equal")), 
+                   type.frame="finite", src.frame="att.frame",
+                   att.frame=sp.finite, xcoord="xcoord", ycoord="ycoord", shapefile=FALSE)
+
+test_that("test equal random selection using an attribute frame and no output shapefile",{
+  expect_true(exists("testsample"))
+  expect_equal(attributes(testsample)$class[1],"SpatialDesign")
+  expect_equal(nrow(testsample@data),10)
 })
