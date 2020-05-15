6447a5e8d6a0703a3a1feab9a748f7ab9f4c9fa0
Olsen <Olsen.Tony@epa.gov>
2020-05-15 11:31:34 -0700

    Warning messages included and include sites with ip = 1.

--- R/pickFiniteSamplePoints.R
+++ R/pickFiniteSamplePoints.R
@@ -2,6 +2,7 @@
 # Function: pickFiniteSamplePoints
 # Programmer: Tom Kincaid
 # Date: May 15, 2019
+# Revised: May 5, 2020 Tony Olsen to include sites with ip = 1
 #
 #' Select Sample Points from a Simple Features Point Object
 #'
@@ -20,6 +21,12 @@
 #'
 #' @param sfobject The sf point object.
 #'
+#' @param warn.ind  A logical value where TRUE indicates a warning message.
+#'   Used for internal collection of messages only.
+#'
+#' @param warn.df A data frame containing messages warning of potential issues.
+#'   Used for internal collection of messages only.
+#'
 #' @return Vector containing feature IDs of the selected sample points.
 #'
 #' @author Tom Kincaid \email{Kincaid.Tom@epa.gov}
@@ -29,9 +36,10 @@
 #' @export
 ################################################################################
 
-pickFiniteSamplePoints <- function(rdx, xc, yc, dx, dy, sfobject) {
+pickFiniteSamplePoints <- function(rdx, xc, yc, dx, dy, sfobject,
+                                   warn.ind = warn.ind, warn.df = warn.df) {
 
-# Determine feature IDs for selected sample points
+  # Determine feature IDs for selected sample points
 
   id <- NULL
   coords <- st_coordinates(sfobject)
@@ -39,20 +47,38 @@ pickFiniteSamplePoints <- function(rdx, xc, yc, dx, dy, sfobject) {
     xr <- c( xc[i] - dx, xc[i])
     yr <- c( yc[i] - dy, yc[i])
     tstcell <- (xr[1] < coords[,1]) & (coords[,1] <= xr[2]) &
-               (yr[1] < coords[,2]) & (coords[,2] <= yr[2])
+      (yr[1] < coords[,2]) & (coords[,2] <= yr[2])
     npt.sample <- sum(rdx == i)
     npt.cell <- length(sfobject$id[tstcell])
     if(npt.sample > npt.cell) {
       id <- c(id, sfobject$id[tstcell])
-      warning(paste("\nThe number of points to be selected from the cell with index value", i, "exceeded the number of points in the cell.\n"))
+      warn <- paste("The number of points to be selected from the cell with index value", i,
+                    "exceeded the number of points in the cell.")
+      if(warn.ind){
+        warn.df <- rbind(warn.df, data.frame(func = I("pickFiniteSamplePoints.R"),
+                                             warning = warn))
+      } else {
+        warn.ind <- TRUE
+        warn.df <- data.frame(func = I("pickFiniteSamplePoints.R"), warning = warn)
+      }
+
     } else if(npt.sample == npt.cell) {
       id <- c(id, sfobject$id[tstcell])
     } else {
-      id <- c(id, sample(sfobject$id[tstcell], npt.sample,
-        prob=sfobject$mdm[tstcell]))
+      nip1 <- 0
+      tst1 <- sfobject$ip[tstcell] == 1
+      if(any(tst1)){
+        nip1 <- sum(tst1)
+        id <- c(id, sfobject$id[tstcell & sfobject$ip == 1])
+      }
+      if( nip1 < npt.sample) {
+        id <- c(id, sample(sfobject$id[tstcell & sfobject$ip < 1],
+                           npt.sample - nip1,
+                           prob = sfobject$ip[tstcell & sfobject$ip < 1]))
+      }
     }
   }
 
-# Return the vector of feature IDs
+  # Return the vector of feature IDs
   return(id)
 }
