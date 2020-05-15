2ce8392771fe37a1777714a1262b6c3736ee4be3
Olsen <Olsen.Tony@epa.gov>
2020-05-15 11:34:27 -0700

    Use of raster function to speed up make.grid function.

--- R/make_grid.R
+++ R/make_grid.R
@@ -1,7 +1,7 @@
 ################################################################################
 # Function: make_grid
-# Programmer: Tom Kincaid
-# Date: July 2, 2019
+# Programmers: Tom Kincaid, Marc Weber
+# Date: October 23, 2019
 #
 #' Create the Grid for a GRTS Survey Design
 #'
@@ -25,20 +25,29 @@
 #'
 #' @export
 ################################################################################
+# in progress function
+# make_grid <- function(xc, yc, dx, dy, sfobject) {
+#   n = length(xc)
+#   grd <- raster(ncol=sqrt(n), nrow=sqrt(n), xmn=xc[1]-dx, xmx=xc[n], ymn=yc[1]-dy, ymx=yc[n])
+#   projection(grd) <- st_crs(sfobject)$proj4string
+#   grd[] <- 1:ncell(grd)
+#   return(grd)
+# }
 
-make_grid <- function(xc, yc, dx, dy, sfobject) {
 
-  n = length(xc)
-  grd = vector("list", n)
-  cell = function(x, dx, y, dy) {
-    st_polygon(list(matrix(c(x-dx, x, x, x-dx, x-dx, y-dy, y-dy, y, y, y-dy), 5)))
-  }
-  for (i in 1:n) {
-    grd[[i]] = cell(xc[i], dx, yc[i], dy)
-  }
-  grd <- st_sfc(grd, crs = st_crs(sfobject))
-  grd <- st_sf(poly = 1:n, geometry = grd)
-  st_agr(grd) <- "constant"
-  return(grd)
-}
-      
+#  working function
+ make_grid <- function(xc, yc, dx, dy, sfobject) {
+
+   n = length(xc)
+   grd = vector("list", n)
+   cell = function(x, dx, y, dy) {
+     st_polygon(list(matrix(c(x-dx, x, x, x-dx, x-dx, y-dy, y-dy, y, y, y-dy), 5)))
+   }
+   for (i in 1:n) {
+     grd[[i]] = cell(xc[i], dx, yc[i], dy)
+   }
+   grd <- st_sfc(grd, crs = st_crs(sfobject))
+   grd <- st_sf(poly = 1:n, geometry = grd)
+   st_agr(grd) <- "constant"
+   return(grd)
+ }
