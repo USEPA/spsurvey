54bd957bc7981a68ba7f81ac12aba33bfc6eecc0
Tom Kincaid <Kincaid.Tom@epa.gov>
2020-10-23 16:46:16 -0700

    Replaced prior version of function.

--- R/warnprnt.R
+++ R/warnprnt.R
@@ -1,35 +1,45 @@
 ################################################################################
 # Function: warnprnt
-# Programmer: Tony Olsen
-# Date: May 1, 2020
+# Programmer: Tom Kincaid
+# Date: June 18, 2020
 #
-#' Internal Function: Print the warnings data Frame that contains errors for the design.
+#' Internal Function: Print the Warnings Data Frame
+#'
+#' This function prints the warnings data frame.
 #'
 #' @param warn.df Data frame that contains warning messages.  The default is
 #'   "warn.df", which is the name given to the warnings data frame created by
 #'   functions in the spsurvey package.
 #'
-#' @param m Vector of indices for stop messages that are to be printed. The
+#' @param m Vector of indices for warning messages that are to be printed. The
 #'   default is a vector containing the integers from 1 through the number of
-#'   rows in stop.df, which will print all stop messages in the data frame.
+#'   rows in warn.df, which will print all warning messages in the data frame.
 #'
-#' @return Invisible return.  Prints warning messages
+#' @return Invisible return.  Prints warnings.
 #'
-#' @author Tony Olsen \email{Olsen.Tony@epa.gov}
+#' @author Tom Kincaid \email{Kincaid.Tom@epa.gov}
 #'
 #' @export
 ################################################################################
 
 warnprnt <- function(warn.df=get("warn.df", envir = .GlobalEnv),
-                     m = 1:nrow(warn.df)) {
+   m = 1:nrow(warn.df)) {
 
-  for(i in m) {
-    cat(paste("Message", i, ": Stratum:", warn.df$stratum[i],
-              "Function: ", warn.df$func[i], "\n"))
-    cat(paste("     ", warn.df$warning[i], "\n"))
-  }
+   for(i in m) {
+      cat(paste0("Warning Message ", i, ":\n"))
+      cat(paste("Function:", warn.df$func[i], "\n"))
+      if(!is.na(warn.df$subpoptype[i]))
+         cat(paste("Population Type:", warn.df$subpoptype[i], "\n"))
+      if(!is.na(warn.df$subpop[i]))
+         cat(paste("Subpopulation:", warn.df$subpop[i], "\n"))
+      if(!is.na(warn.df$indicator[i]))
+         cat(paste("Indicator:", warn.df$indicator[i], "\n"))
+      if(!is.na(warn.df$stratum[i]))
+         cat(paste("Stratum:", warn.df$stratum[i], "\n"))
+      cat(paste("Warning:", warn.df$warning[i]))
+      cat(paste("Action:", warn.df$action[i], "\n"))
+   }
 
-  invisible(NULL)
-
+   invisible(NULL)
 }
 
