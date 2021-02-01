25ff415f09dcf148060e36157e820bd2d0cab0ad
Marc H. Weber <mweber36@gmail.com>
2021-02-01 08:05:36 -0800

    get rid of parallel in spsurvey.R to update Namespace

--- R/spsurvey.R
+++ R/spsurvey.R
@@ -36,8 +36,6 @@
 #' @importFrom grDevices graphics.off pdf rainbow
 #' @importFrom Hmisc describe
 #' @importFrom MASS ginv
-#' @importFrom parallel clusterEvalQ clusterExport detectCores getDefaultCluster
-#'   makeCluster parLapply setDefaultCluster stopCluster
 #' @importFrom rgeos gIntersection
 #' @importFrom sp SpatialPoints SpatialPointsDataFrame
 #' @importFrom stats addmargins chisq.test confint dist dnorm ftable model.frame
