57bb0a85de2edcbdcd9b71660bc37d49bd4ce5ed
Michael Dumelle <mjdumelle@gmail.com>
2021-03-31 16:42:57 -0700

    adjusted to accomodate unequal inclusion probabilities

--- R/spbalance.R
+++ R/spbalance.R
@@ -12,18 +12,29 @@
 #' applicable for unstratified or stratified designs with equal selection
 #' probabilities.
 #'
-#' @param object A design object output from \code{grts()} or \code{irs()}
-#' having class \code{"design"}.
-#' @param sframe An \code{sframe} or \code{sf} object.
-#' @param sites The sites in object for which spatial balance metrics
-#' are desired. This argument must be \code{"sites_base"}, \code{"sites_over"}, or
-#' \code{"sites_near"}. Defaults to \code{"sites_base"}.
+#' @param object An \code{sf} object containig the sampled sites.
+#' 
+#' @param sframe An \code{sframe} or \code{sf} object containing the sample frame.
+#' 
+#' @param stratum_var The name of the stratum variable in \code{object} 
+#' and \code{sframe}. If \code{NULL} (the default), no strata is assumed.
+#' If a single character vector is provided, it is assumed this is the 
+#' name of the stratum variable in \code{object} and \code{sframe}. If
+#' a two-dimensional character vector is provided, one element must be
+#' named "object" and corresponds to the name of the stratum variable
+#' in \code{object}, while the other element must be named "sframe" and
+#' correponds to the name of the stratum variable in \code{sframe}.
+#' 
+#' @param ip Inclusion probabilities associated with each row of \code{sframe}.
+#' If these are not provided, an equal probability design is assumed (within
+#' strata).
+#' 
 #' @param metrics A character vector of spatial balance metrics:
 #' \itemize{
 #'   \item{pielou - }{pielou evenness index (the default)}
 #'   \item{simpsons - }{simpsons evenness index}
 #'   \item{chisq - }{chi-squared loss}
-#'   \item{pielou - }{absolute error loss}
+#'   \item{abserr - }{absolute error loss}
 #'  }
 #'
 #' @param extents Should the total extent within each dirichlet
@@ -38,21 +49,37 @@
 #' spbalance(sample, NE_Lakes)
 #' }
 #' ###############################################################################
-spbalance <- function(object, sframe, sites = "sites_base", metrics = "pielou", extents = FALSE) {
-  object_split <- split(object[[sites]], object[[sites]][["stratum"]])
-  if (is.null(object$dsgn$Call$stratum_var)) {
-    output <- lapply("None", function(x) {
-      calculate_spbalance(object_split[[x]], sframe, sites, metrics, extents)
-    })
-    names(output) <- "None"
+spbalance <- function(object, sframe, stratum_var = NULL, ip = NULL, metrics = "pielou", extents = FALSE) {
+  
+  if (is.null(stratum_var)) {
+    object$stratum_var <- "None"
+    sframe$stratum_var <- "None"
+    stratum_var <- "stratum_var"
+  } 
+  
+  if (length(stratum_var) == 1) {
+    stratum_var <- c(object = stratum_var, sframe = stratum_var)
+  } else if (length(stratum_var) == 2) {
+    if (!all(names(stratum_var) %in% c("object", "sframe"))) {
+      stop("names of stratum_var must be object and sframe")
+    }
   } else {
-    sframe_split <- split(sframe, sframe[[object$dsgn$stratum_var]])
-    output <- lapply(object$dsgn$stratum, function(x) {
-      calculate_spbalance(object_split[[x]], sframe_split[[x]], sites, metrics, extents)
-    })
-    names(output) <- object$dsgn$stratum
+    stop("length of stratum_var must be zero, one, or two")
   }
-  metrics_dfs <- lapply(names(output), function(x) cbind(stratum = x, data.frame(metric = metrics, value = output[[x]]$values)))
+  
+  object_levels <- sort(unique(object[[stratum_var["object"]]]))
+  sframe_levels <- sort(unique(sframe[[stratum_var["sframe"]]]))
+  
+  if (!all.equal(object_levels, sframe_levels)) {
+    stop("stratum_var levels for object and sframe must be the same")
+  }
+  
+  object_split <- split(object, object[[stratum_var["object"]]])
+  sframe_split <- split(sframe, sframe[[stratum_var["sframe"]]])
+  
+  output <- lapply(object_levels, function(x) calculate_spbalance(object_split[[x]], sframe_split[[x]], ip, metrics, extents))
+  names(output) <- object_levels
+  metrics_dfs <- lapply(object_levels, function(x) cbind(stratum = x, data.frame(metric = metrics, value = output[[x]]$values)))
   metrics <- do.call("rbind", metrics_dfs)
   metrics <- metrics[order(metrics$metric), , drop = FALSE]
   row.names(metrics) <- NULL
@@ -66,18 +93,29 @@ spbalance <- function(object, sframe, sites = "sites_base", metrics = "pielou", 
   } else {
     return(metrics)
   }
-  output
-}
+} 
 
-
-calculate_spbalance <- function(object_split, sframe_split, sites, metrics, extents) {
-
+calculate_spbalance <- function(object_split, sframe_split, ip, metrics, extents) {
+  
+  
+  if (all(st_geometry_type(sframe_split) %in% c("POINT", "MULTIPOINT"))) {
+    sframe_split$dens <- 1
+  } else if (all(st_geometry_type(sframe_split) %in% c("LINESTRING", "MULTILINESTRING"))) {
+    sframe_split$dens <- as.numeric(st_length(sframe_split))
+  } else if (all(st_geometry_type(sframe_split) %in% c("POLYGON", "MULTIPOLYGON"))) {
+    sframe_split$dens <- as.numeric(st_area(sframe_split))
+  }
+  
+  if (is.null(ip)) {
+    sframe_split$ip <- nrow(object_split) * (sframe_split$dens / sum(sframe_split$dens))
+  }
+  
   # finding the sframe bounding box, this needs to be reordered to
   # use in the Dirichlet Tesselation
   pop_bbox <- st_bbox(sframe_split)[c("xmin", "xmax", "ymin", "ymax")]
-
+  
   # working with the objectd sites
-
+  
   ## take the objectd sites coordinates
   samp_coords <- st_coordinates(object_split)
   ## name them X and Y
@@ -88,51 +126,49 @@ calculate_spbalance <- function(object_split, sframe_split, sites, metrics, exte
   samp_ycoord <- samp_coords[, "Y"]
   ## recover the object size
   n <- nrow(object_split)
-
+  
   # and the sframe sites
-
-
+  
+  
   ## recover the sframe size
-  N <- nrow(sframe_split)
-
+  # N <- nrow(sframe_split)
+  
   # spatial balance with respect to the sframe
-
+  
   tiles <- tile.list(deldir(x = samp_xcoord, y = samp_ycoord, rw = pop_bbox))
-
+  
   ## using lapply instead of a loop
   sftess <- lapply(tiles, get_sftess)
   sftess <- st_sfc(sftess, crs = st_crs(sframe_split))
   sftess <- st_sf(poly = 1:n, geometry = sftess)
-  sftess <- st_join(sframe_split, sftess)
-
-  # calculate counts
-
-  ## for points, sframe geometry must be point or multipoint
+  sftess <- suppressWarnings(st_intersection(st_make_valid(sframe_split), sftess))
+  
+  
+  # for points, sframe geometry must be point or multipoint
   if (all(st_geometry_type(sframe_split) %in% c("POINT", "MULTIPOINT"))) {
     ## storing a dummy variable to index counts by
-    sftess$point_mdm <- 1
-    ## summing over each polygon
-    extent <- with(sftess, tapply(point_mdm, poly, sum))
-    ## setting NA's equal to zero
-    extent[is.na(extent)] <- 0
+    sftess$adjip <- sftess$ip
   } else if (all(st_geometry_type(sframe_split) %in% c("LINESTRING", "MULTILINESTRING"))) {
-    sftess$length_mdm <- as.numeric(st_length(sftess))
-    extent <- with(sftess, tapply(length_mdm, poly, sum))
-    extent[is.na(extent)] <- 0
+    sftess$polydens <- as.numeric(st_length(sftess))
+    sftess$adjip <- sftess$ip * sftess$polydens / sftess$dens
   } else if (all(st_geometry_type(sframe_split) %in% c("POLYGON", "MULTIPOLYGON"))) {
-    sftess$area_mdm <- as.numeric(st_area(sftess))
-    extent <- with(sftess, tapply(area_mdm, poly, sum))
-    extent[is.na(extent)] <- 0
+    sftess$polydens <- as.numeric(st_area(sftess))
+    sftess$adjip <- sftess$ip * sftess$polydens / sftess$dens
   }
-
+  
+  ## summing over each polygon
+  extent <- with(sftess, tapply(adjip, poly, sum))
+  ## setting NA's equal to zero
+  extent[is.na(extent)] <- 0
+  
   # making proportions and expected quantities
   proportions <- extent / sum(extent)
   expected_proportions <- 1 / n
-
-
+  
+  
   # metrics
   values <- vapply(metrics, calculate_metric, double(1), proportions, expected_proportions)
-
+  
   # returning the results when stored by the user
   output <- list(values = values)
   if (extents) {
@@ -144,7 +180,7 @@ calculate_spbalance <- function(object_split, sframe_split, sites, metrics, exte
 get_sftess <- function(tile) {
   ## finding the number of points in the bounding polygon
   npol <- length(tile$x)
-
+  
   ## creating and returning the appropriate polygon after binding coords
   st_polygon(list(cbind(
     c(tile$x[1], tile$x[npol:1]),
@@ -155,11 +191,11 @@ get_sftess <- function(tile) {
 
 calculate_metric <- function(metric, proportions, expected_proportions) {
   switch(metric,
-    pielou = calculate_pielou(proportions, expected_proportions),
-    chisq = calculate_chisq(proportions, expected_proportions),
-    abserr = calculate_abserr(proportions, expected_proportions),
-    simpsons = calculate_simpsons(proportions, expected_proportions),
-    stop("an invalid metric was provided")
+         pielou = calculate_pielou(proportions, expected_proportions),
+         chisq = calculate_chisq(proportions, expected_proportions),
+         abserr = calculate_abserr(proportions, expected_proportions),
+         simpsons = calculate_simpsons(proportions, expected_proportions),
+         stop("an invalid metric was provided")
   )
 }
 
