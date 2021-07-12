b2cf7918ebba2b943eecadb498974e8315b6872e
Michael Dumelle <mjdumelle@gmail.com>
2021-07-12 08:42:37 -0700

    adjwgt adjustments (setting some default values)

--- R/adjwgt.R
+++ R/adjwgt.R
@@ -10,21 +10,25 @@
 #'   implementation results in the use of oversample sites or if it is desired
 #'   to have final weights sum to the known frame size.  Adjusted weights are
 #'   equal to \code{initial_weight * framesize/sum(initial_weight)}.  The adjustment
-#'   is done separately for each category specified in \code{wtcat}.
-#'
-#' @param sites Vector indicating site use; \code{TRUE} indicates the site
-#'   should be inlcuded in the weight adjustment and \code{FALSE} indicates
-#'   the site should not be included in the weight adjustment.
+#'   is done separately for each category specified in \code{wgtcat}.
 #'
 #' @param wgt Vector of initial weights for each site. These equal
 #'   the reciprocal of the sample inclusion probabilities.
 #'
-#' @param wtcat Vector containing each site's weight adjustment
-#'   category name.
+#' @param wgtcat Vector containing each site's weight adjustment
+#'   category name. The default is \code{NULL}, which assumes every
+#'   site is in the same category.
 #'
 #' @param framesize Vector containing the known size of the frame
-#'   for each category name in wtcat. The names attribute much match the
-#'   category names in \code{wtcat}.
+#'   for each category name in wgtcat. If \code{wgtcat} is provided,
+#'   the names in \code{framesize} must match the names in \code{wgtcat}.
+#'   If \code{wgtcat} is not provided, an unnamed scalar is given to
+#'   \code{framesize}.
+#'
+#' @param sites Vector indicating site use; \code{TRUE} indicates the site
+#'   should be included in the weight adjustment and \code{FALSE} indicates
+#'   the site should not be included in the weight adjustment. The default is
+#'   \code{NULL}, which assumes every site should be included.
 #'
 #' @return Vector of adjusted weights, where the adjusted weight is set
 #'   to \code{0} for sites whose value in the sites argument was set to
@@ -35,23 +39,35 @@
 #' @keywords survey misc
 #'
 #' @examples
-#' sites <- as.logical(rep(rep(c("TRUE", "FALSE"), c(9, 1)), 5))
 #' wgt <- runif(50, 10, 100)
-#' wtcat <- rep(c("A", "B"), c(30, 20))
-#' framesize <- c(15, 10)
-#' names(framesize) <- c("A", "B")
-#' adjwgt(sites, wgt, wtcat, framesize)
+#' wgtcat <- rep(c("A", "B"), c(30, 20))
+#' framesize <- c(A = 15, B = 10)
+#' sites <- rep(rep(c(TRUE, FALSE), c(9, 1)), 5)
+#' adjwgt(wgt, wgtcat, framesize, sites)
 #' @export
-adjwgt <- function(sites, wgt, wtcat, framesize) {
+adjwgt <- function(wgt, wgtcat = NULL, framesize, sites = NULL) {
 
-  # Sum initial weights by wtcat for evaluated sites
+  # set default wgtcat argument if omitted
 
-  wgtsum <- tapply(wgt[sites], wtcat[sites], sum)
+  if (is.null(wgtcat)) {
+    wgtcat <- rep("wgtgrp", length(wgt))
+    names(framesize) <- "wgtgrp"
+  }
+
+  # set default sites argument if omitted
+
+  if (is.null(sites)) {
+    sites <- rep(TRUE, length(wgt))
+  }
+
+  # Sum initial weights by wgtcat for evaluated sites
+
+  wgtsum <- tapply(wgt[sites], wgtcat[sites], sum)
 
   # Adjustment factor to be applied to weights for adjustment category
 
   adjfac <- framesize / wgtsum[match(names(framesize), names(wgtsum))]
-  wtadj <- adjfac[match(wtcat, names(adjfac))]
+  wtadj <- adjfac[match(wgtcat, names(adjfac))]
   adjwgt <- wgt * wtadj
   adjwgt[!sites] <- 0
   as.vector(adjwgt)
