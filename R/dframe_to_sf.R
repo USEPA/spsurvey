2596fc801ed63f0a9c8ccbcb3fb9b6024ff7087c
Michael Dumelle <mjdumelle@gmail.com>
2021-07-30 12:44:20 -0700

    addressed bug in dframe_to_sf; updated documentation in both

--- R/dframe_to_sf.R
+++ R/dframe_to_sf.R
@@ -4,10 +4,12 @@
 # Date: May 19, 2021
 #' Transform a dframe object into an sf object or a data frame object
 #'
-#' @description \code{dframe_to_sf()} transforms an sframe object into an sf object,
+#' @description \code{dframe_to_sf()} transforms an sframe object into an \code{sf} object,
 #' which may be useful when calling generic functions with sf class (e.g. mapview).
-#' \code{dframe_to_df()} transforms a dframe object into an data frame object,
-#' while retaining the remaining structure of dframe (e.g. if dframe is also a tibble,
+#' \code{dframe_to_df()} transforms a \code{dframe} object into an \code{data.frame} object. If
+#' the \code{dframe} object is also an \code{sf} object, \code{dframe_to_df()} stores the
+#' coordinates as columns in the new \code{data.frame} object.
+#' The remaining structure of \code{dframe} stays the same (e.g. if \code{dframe} is also a tibble,
 #' it will remain a tibble after \code{dframe_to_df}).
 #'
 #' @param object A \code{dframe} object.
@@ -17,11 +19,16 @@
 #'
 #' @return An \code{sf} object (\code{dframe_to_sf()}) or a data frame object (\code{dframe_to_df()}).
 #'
+#' @importFrom sf st_as_sf
+#'
 #' @seealso dframe
 #'
 #' @export
 ###############################################################################
 dframe_to_sf <- function(object, ...) {
+  if (!inherits(object, "dframe")) {
+    stop("object must have class dframe")
+  }
   if (inherits(object, "sf")) {
     new_object <- structure(object, class = c("sf", setdiff(class(object), c("dframe", "sf"))))
   } else {
@@ -32,7 +39,16 @@ dframe_to_sf <- function(object, ...) {
 
 #' @describeIn dframe_to_sf
 #'
+#' @importFrom sf st_coordinates st_drop_geometry
+#'
 #' @export
 dframe_to_df <- function(object) {
-  new_object <- structure(object, class = c(setdiff(class(object), "data.frame"), "data.frame"))
+  if (!inherits(object, "dframe")) {
+    stop("object must have class dframe")
+  }
+  if (inherits(object, "sf")) {
+    coords <- st_coordinates(object)
+    object <- cbind(st_drop_geometry(object), coords) # drop geometry removes sf class
+  }
+  new_object <- structure(object, class = c("data.frame", setdiff(class(object), c("dframe", "data.frame"))))
 }
--- R/sframe_to_sf.R
+++ R/sframe_to_sf.R
@@ -4,7 +4,7 @@
 # Date: May 19, 2021
 #' Transform an sframe object into an sf object
 #'
-#' @description \code{sframe_to_sf()} transforms an sframe object into an sf object,
+#' @description \code{sframe_to_sf()} transforms an \code{sframe} object into an \code{sf} object,
 #' which may be useful when calling generic functions with sf class (e.g. mapview)
 #'
 #' @param object An \code{sframe} object.
