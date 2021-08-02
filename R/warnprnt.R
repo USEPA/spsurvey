84fd5ec2c01327f89b7b74d5834ef26969c18a59
Michael Dumelle <mjdumelle@gmail.com>
2021-08-02 16:30:45 -0700

    updated warning print functions

--- R/warnprnt.R
+++ R/warnprnt.R
@@ -3,9 +3,9 @@
 # Programmer: Tom Kincaid
 # Date: June 18, 2020
 #
-#' Internal Function: Print the Warnings Data Frame
+#' Print the Warnings
 #'
-#' This function prints the warnings data frame.
+#' This function prints the warnings.
 #'
 #' @param warn_df Data frame that contains warning messages.  The default is
 #'   \code{"warn_df"}, which is the name given to the warnings data frame created by
@@ -15,7 +15,7 @@
 #'   default is a vector containing the integers from 1 through the number of
 #'   rows in \code{warn_df}, which will print all warning messages in the data frame.
 #'
-#' @return Invisible return.  Prints warnings.
+#' @return Printed warnings.
 #'
 #' @author Tom Kincaid \email{Kincaid.Tom@@epa.gov}
 #'
@@ -24,23 +24,35 @@
 
 warnprnt <- function(warn_df = get("warn_df", envir = .GlobalEnv),
                      m = 1:nrow(warn_df)) {
+
+  # old front code in warndsgn
+  # for (i in m) {
+  #   cat(paste(
+  #     "Message", i, ": Stratum:", warn_df$stratum[i],
+  #     "Function: ", warn_df$func[i], "\n"
+  #   ))
+  #   cat(paste("     ", warn_df$warning[i], "\n"))
+  # }
+
   for (i in m) {
     cat(paste0("Warning Message ", i, ":\n"))
-    cat(paste("Function:", warn_df$func[i], "\n"))
-    if (!is.na(warn_df$subpoptype[i])) {
+    # cat(paste("Function:", warn_df$func[i], "\n")) # removed function name output
+    if (sum(!is.null(warn_df$subpoptype[i]), is.na(warn_df$subpoptype[i]))) {
       cat(paste("Population Type:", warn_df$subpoptype[i], "\n"))
     }
-    if (!is.na(warn_df$subpop[i])) {
+    if (sum(!is.null(warn_df$subpop[i]), is.na(warn_df$subpop[i]))) {
       cat(paste("Subpopulation:", warn_df$subpop[i], "\n"))
     }
-    if (!is.na(warn_df$indicator[i])) {
+    if (sum(!is.null(warn_df$indicator[i]), is.na(warn_df$indicator[i]))) {
       cat(paste("Indicator:", warn_df$indicator[i], "\n"))
     }
-    if (!is.na(warn_df$stratum[i])) {
+    if (sum(!is.null(warn_df$stratum[i]), is.na(warn_df$stratum[i]))) {
       cat(paste("Stratum:", warn_df$stratum[i], "\n"))
     }
     cat(paste("Warning:", warn_df$warning[i]))
-    cat(paste("Action:", warn_df$action[i], "\n"))
+    if (sum(!is.null(warn_df$action[i]), is.na(warn_df$action[i]))) {
+      cat(paste("Action:", warn_df$action[i], "\n"))
+    }
   }
 
   invisible(NULL)
